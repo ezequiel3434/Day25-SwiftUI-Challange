9e3538a5c50a8840d5a58ee3a525fc6649f70a25
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-11 10:32:55 -0300

    challange done

--- Day25-SwiftUI-Challange/ContentView.swift
+++ Day25-SwiftUI-Challange/ContentView.swift
@@ -9,8 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    var things = ["Rock","Paper","Scissors"]
+    @State private var choice = "Paper"
+    @State private var shouldWin = false
+    @State private var score = 0
+   
+    
+    
+    
+    func nextRound() {
+        choice = things[Int.random(in: 0...2)]
+        shouldWin = Bool.random()
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        
+
+               
+        VStack {
+            
+            Text("My Choice is \(choice)")
+            Text("You must \(shouldWin ? "WIN" : "LOSE")")
+            
+//            ForEach(things, id: \.self) {
+//                Button($0){}
+//            }
+            
+            
+            
+            Button(things[0]) {
+                if self.choice == self.things[2] && self.shouldWin == true || self.choice == self.things[1] && self.shouldWin == false {
+                    self.score += 1
+                } else {
+                    self.score -= 1
+                }
+                self.nextRound()
+            }
+            Button(things[1]) {
+                if self.choice == self.things[0] && self.shouldWin == true || self.choice == self.things[2] && self.shouldWin == false {
+                    self.score += 1
+                } else {
+                    self.score -= 1
+                }
+                self.nextRound()
+
+            }
+            Button(things[2]) {
+                if self.choice == self.things[1] && self.shouldWin == true || self.choice == self.things[0] && self.shouldWin == false {
+                    self.score += 1
+                } else {
+                    self.score -= 1
+                }
+
+                self.nextRound()
+            }
+            Text("Your score is \(score)")
+        }
     }
 }
 
